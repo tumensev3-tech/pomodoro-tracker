7c32cd0289254c7bf17c7a52664c8983c2653265
tumensev3-tech <tumensev3@gmail.com>
2026-09-25 10:20:42 +0600

    Apply project icon migration

--- app/src/main/java/com/drklo/pomodoro/data/AppContainer.kt
+++ app/src/main/java/com/drklo/pomodoro/data/AppContainer.kt
@@ -27,7 +27,8 @@ class AppContainer(context: Context) {
     ).addMigrations(
         AppDatabase.MIGRATION_1_2,
         AppDatabase.MIGRATION_2_3,
-        AppDatabase.MIGRATION_3_4
+        AppDatabase.MIGRATION_3_4,
+        AppDatabase.MIGRATION_4_5
     ).build()
 
     val projectRepository: ProjectRepository by lazy { ProjectRepository(database) }
